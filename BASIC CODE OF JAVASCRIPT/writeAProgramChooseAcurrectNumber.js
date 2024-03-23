ce86d5159d5bce45d822f0704a6f6992b285e25c
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-23 23:33:40 +0530

    best program

---
+++ BASIC CODE OF JAVASCRIPT/writeAProgramChooseAcurrectNumber.js
@@ -0,0 +1,7 @@
+let gameNum = 20;
+let userNum = prompt("guess the game number:")
+
+while(userNum != gameNum){
+    userNum = prompt("please guess again game number: ");
+}
+console.log("you are guess correct game number :");
