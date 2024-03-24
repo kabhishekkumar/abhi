f935b65df40492dada31ec822003799cc80569d8
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-24 21:52:51 +0530

    print the number is divisible by three and two

---
+++ LOOPS JS/printTheNumberThatDivisibleByThreeOrNot.js
@@ -0,0 +1,12 @@
+for(let i=1; i<=100; i++){
+    if(i%3==0){
+        console.log("this number is divisible by three = ",i);
+       
+    }
+}
+
+for(let i = 1; i<=100; i++){
+    if(i%2==0){
+        console.log("this number is divisible by two = ",i)
+    }
+}
