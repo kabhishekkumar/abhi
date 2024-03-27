383e0da7c74e046dcac55be119b01688f4ade9f0
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-27 16:39:07 +0530

    print the discount price of book or object

---
+++ LOOPS JS/findTheDiscountOfSomeObject.js
@@ -0,0 +1,10 @@
+let items = [32,62,54,92,74,25];
+let i= 0;
+for(let val of items){
+
+    
+    let c = val/10;
+    items[i]= items[i]-c;
+    console.log(`value of index ${i}= ${items[i]}`)
+    i++;
+}
