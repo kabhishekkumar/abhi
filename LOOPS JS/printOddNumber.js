7f31f5afd5897785f1f263ba9d6fa62e5774b2ea
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-28 21:12:50 +0530

    print odd number between one to hundred

--- LOOPS JS/printOddNumber.js
+++ LOOPS JS/printOddNumber.js
@@ -1,6 +1,5 @@
 for(let i=1; i<=100; i++){
     if(i%2!=0){
-        console.log("print this number is odd number = ",i);
-       //  console.log(`print this number ${i}`)
+        console.log(" this number is odd number = ",i);
     }
 }
